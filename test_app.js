78958de57afa6cd895dcd2d59e1e3ecade9bb696
davidexco <david.roberin@ex.co>
2023-09-18 13:14:58 +0300

    added sample get route

--- test_app.js
+++ test_app.js
@@ -4,6 +4,11 @@ app.get('/', function (req, res) {
 	  res.send('Hello World!');
 });
 
+app.get('/sendmessage', (req, res) => {
+	console.log('sendmessage', req.query.msg);
+	return res.status(200).send({});
+});
+
 app.listen(3001, function () {
 	console.log('App started on 3001');
 });
